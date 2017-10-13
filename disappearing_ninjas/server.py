84a3fb950c24e5b02b04b7b56a2b989c7f1c1687
Beth Hart <bethhart27@gmail.com>
2017-10-13 03:20:41 +0000

    dry up code

--- disappearing_ninjas/server.py
+++ disappearing_ninjas/server.py
@@ -7,23 +7,21 @@ def home():
     
 @app.route('/ninja')
 def group():
-    images = ["/static/images/leonardo.jpg",
-    "/static/images/raphael.jpg", "/static/images/michelangelo.jpg",
-        "/static/images/donatello.jpg"]
+    images = ["leonardo", "raphael", "michelangelo", "donatello"]
     return render_template("ninjas.html", images=images)
     
 @app.route("/ninja/<color>")
 def solo(color):
     if color == "blue":
-        images=["/static/images/leonardo.jpg"]
+        images=["leonardo"] 
     elif color == "red":
-        images=["/static/images/raphael.jpg"]
+        images=["raphael"] 
     elif color == "orange":
-        images=["/static/images/michelangelo.jpg"]
+        images=["michelangelo"] 
     elif color == "purple":
-        images=["/static/images/donatello.jpg"]
+        images=["donatello"] 
     else:
-        images=["/static/images/notapril.jpg"]
+        images=["notapril"] 
     return render_template("ninjas.html", images=images)
 
 app.run(host="0.0.0.0", port=int("8080"), debug=True)
