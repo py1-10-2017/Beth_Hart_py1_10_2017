7726446c0d9f072db4156e539cebca21fa551d3f
Beth Hart <bethhart27@gmail.com>
2017-10-13 00:56:57 +0000

    corrected redirect functionality

--- number_guessing_game/server.py
+++ number_guessing_game/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, redirect, session, request, flash
 import random
 app = Flask(__name__)
 app.secret_key ="going west"
@@ -6,25 +6,31 @@ app.secret_key ="going west"
 
 @app.route('/', methods=["get", "post"])
 def home():
- 
-    session['num'] = random.randrange(1, 101)
+    if "num" not in session:
+        session['num'] = random.randrange(1, 101)
     print session['num']
     return render_template("index.html")
 
 @app.route('/guess', methods=["post", "get"])
 def guess():
+    if not request.form['guess'].isdigit():
+        flash("invalid entry -- enter only numbers")
+        return redirect('/')
     guess = int(request.form['guess'])
     # print type(session["num"]) //ERROR as to why msg did not print
     # print type(guess)
     if guess == session['num']:
-        msg = "you guessed it!"
+        session["msg"] = "you guessed it!"
     elif guess > session['num']:
-        msg = "your guess is too high"
+        session["msg"] = "your guess is too high"
     elif guess < session['num']:
-        msg = " your guess is too low"
-    print msg
-    return render_template("index.html", msg=msg)
+        session["msg"] = " your guess is too low"
+    return redirect("/")
 
+@app.route('/reset')
+def play_again():
+    session.clear()
+    return redirect('/')
 
 
 app.run(host="0.0.0.0", port=int("8080"), debug=True)
