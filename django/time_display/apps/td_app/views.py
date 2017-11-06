1ecefb9a0a8773d0a82c9e0ad7a13cbe1972ff4f
Beth Hart <bethhart27@gmail.com>
2017-11-06 16:47:53 +0000

    styling and time format

--- django/time_display/apps/td_app/views.py
+++ django/time_display/apps/td_app/views.py
@@ -3,14 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 import datetime
-from pytz import timezone
-import time
+from time import gmtime, strftime
 
 def index(req):
-    time = datetime.datetime.now()
-
+    # time = datetime.datetime.now()
+    time = gmtime()
+    print (time)
     context = {
-        'time':time
+        'time':strftime("%A %B %d, %Y\n%H:%M %p", time)
     }
 
     return render(req, 'td_app/index.html', context)
